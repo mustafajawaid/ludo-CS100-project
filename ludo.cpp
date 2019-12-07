b4cb3dc062deb051f3003c9218667e9277cef370
mustafajawaid <44783039+mustafajawaid@users.noreply.github.com>
2019-12-07 20:09:41 +0500

    Add files via upload

--- ludo.cpp
+++ ludo.cpp
@@ -15,9 +15,10 @@ int dice() //Simulates dice roll
     return roll;
 }
 
+void PrintBoard();
+
 int main()
 {
-
 
 string ludo[13][13];
 
@@ -52,7 +53,7 @@ for(int i = 8; i<13; i++) //Quadrant 4
 
 
 int initial_i=11, initial_j=5;
-ludo[initial_i][initial_j]="1";
+//ludo[initial_i][initial_j]="1";
 int input;
 
 for(int i = 0; i<13; i++) //Empty Board Printing
@@ -64,14 +65,40 @@ for(int i = 0; i<13; i++) //Empty Board Printing
     cout<<endl;
 }
 
+int roll1;
+do
+{
+    cout<<"\nPress 1 to roll dice: ";
+    cin>>input;
 
-do{
+    roll1=dice(); //Roll 6 to free gotti
+    cout<<"\nNumber rolled is "<<roll1<<endl<<endl;
+
+    if(roll1==6)
+    {
+        ludo[initial_i][initial_j]="1";
+    }
+
+    for(int i = 0; i<13; i++) //Empty Board Printing
+    {
+        for(int j = 0; j<13; j++)
+        {
+            cout << setw(1) << ludo[i][j];
+        }
+    cout<<endl;
+    }
+
+}while(roll1!=6);
+
+cout<<"\nPress 1 to roll dice: ";
+while(cin>>input)
+{
     int roll = dice();
-    cout<<"Number rolled is "<<roll<<endl<<endl;
+    cout<<"\nNumber rolled is "<<roll<<endl<<endl;
 
     if(initial_i-roll<8)
     {
-        ludo[initial_i-4][initial_j-roll+3]="1";
+        ludo[initial_i-4][initial_j-(roll-3)]="1";
         int pos_i=initial_i-4, pos_j=initial_j-roll+3;
     }
 
@@ -99,7 +126,7 @@ do{
     cout<<endl<<endl;
     cout<<"Press 1 to roll dice: ";
 
-}while(cin>>input);
+}
 
 return 0;
 }
